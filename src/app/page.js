4d1a5e27b8aaac0a6d3d792a7d9550be3db8d089
K Layasree <layasree81103@gmail.com>
2024-12-19 15:41:06 +0530

    more changes

--- src/app/page.js
+++ src/app/page.js
@@ -12,15 +12,16 @@ export default function Home() {
 		<div className="space-y-8">
 			<section className="bg-white rounded-lg shadow-xl p-6">
 				<h1 className="text-4xl font-bold mb-4">
-					Community Emergency Response
+					Community Sheild -Hyper local disaster network
 				</h1>
 				<p className="text-gray-600 text-lg">
 					Access critical resources, connect with skilled volunteers, and stay
-					informed during emergencies - even without internet connectivity.
+					informed during emergencies - even without internet connectivity.All you need is Bluetooth connectivity or a wifi direct
 				</p>
 			</section>
 			<ResourceMap />
 			<EmergencyActions />
+			
 		</div>
 	);
 }
